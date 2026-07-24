3903c0f6738623396531c9dc593c0642299c4b04
Nikita Walkowski <valkovskii.n@selectel.ru>
2026-07-24 21:58:00 +0300

    Update repo memstorage

--- internal/repository/memstorage.go
+++ internal/repository/memstorage.go
@@ -1,6 +1,11 @@
 package repository
 
-// Тип для хранения метрик
+import (
+	"fmt"
+	"maps"
+)
+
+// MemStorage implements in-memory metric storage using separate maps for gauge and counter metrics.
 type MemStorage struct {
 	gauge   map[string]float64
 	counter map[string]int64
@@ -8,6 +13,7 @@ type MemStorage struct {
 
 var _ Repository = (*MemStorage)(nil)
 
+// NewMemStorage creates and returns a pointer to an initialized MemStorage instance with empty metric maps.
 func NewMemStorage() *MemStorage {
 	return &MemStorage{
 		gauge:   map[string]float64{},
@@ -15,12 +21,42 @@ func NewMemStorage() *MemStorage {
 	}
 }
 
+// UpdateGauge sets the gauge metric identified by name to the specified value, overwriting any previous value.
 func (ms *MemStorage) UpdateGauge(name string, value float64) error {
 	ms.gauge[name] = value
 	return nil
 }
 
+// UpdateCounter increments the counter metric identified by name by the specified delta value.
 func (ms *MemStorage) UpdateCounter(name string, value int64) error {
 	ms.counter[name] += value
 	return nil
+}
+
+// GetGauge returns the value of the gauge metric identified by name.
+// Returns an error if the metric does not exist.
+func (ms *MemStorage) GetGauge(name string) (value float64, err error) {
+	if _, ok := ms.gauge[name]; ok {
+		return ms.gauge[name], nil
+	}
+	return 0, fmt.Errorf("Gauge %s not found", name)
+}
+
+// GetCounter returns the value of the counter metric identified by name.
+// Returns an error if the metric does not exist.
+func (ms *MemStorage) GetCounter(name string) (value int64, err error) {
+	if _, ok := ms.counter[name]; ok {
+		return ms.counter[name], nil
+	}
+	return 0, fmt.Errorf("Counter %s not found", name)
+}
+
+// GetAllGauges returns a shallow copy of all stored gauge metrics to prevent external mutation of internal state.
+func (ms *MemStorage) GetAllGauges() (m map[string]float64) {
+	return maps.Clone(ms.gauge)
+}
+
+// GetAllCounters returns a shallow copy of all stored counter metrics to prevent external mutation of internal state.
+func (ms *MemStorage) GetAllCounters() (m map[string]int64) {
+	return maps.Clone(ms.counter)
 }
