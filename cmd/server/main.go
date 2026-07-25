76f252745caf4baa79b40aed560810b19981a385
Nikita Walkowski <valkovskii.n@selectel.ru>
2026-07-25 13:45:01 +0300

    Update server main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,21 +7,32 @@ import (
 
 	"github.com/PrometheRus/metricscollector/internal/handler"
 	"github.com/PrometheRus/metricscollector/internal/repository"
+	"github.com/go-chi/chi"
 )
 
 func main() {
-	var r = repository.NewMemStorage()
-	var h = handler.MetricsHandler{Storage: r}
+	var s = repository.NewMemStorage()
+	var h = handler.MetricsHandler{Storage: s}
 
-	mux := http.NewServeMux()
-	mux.Handle("/update/", &h)
+	router := chi.NewRouter()
+
+	router.Get("/", h.GetRootHandler)
+	router.Get("/value/{TYPE}/{METRIC}", h.GetMetricHandler)
+
+	router.Post("/update", h.PostNoTypeHandler)
+	router.Route("/update/{TYPE}", func(r chi.Router) {
+		r.Use(handler.TypeMiddleware)             // проверит TYPE для всех трёх ниже
+		r.Post("/", h.PostNoMetricHandler)        // 404 — нет имени
+		r.Post("/{METRIC}", h.PostNoValueHandler) // 400 — нет значения
+		r.Post("/{METRIC}/{VALUE}", h.PostFullHandler)
+	})
 
 	srv := &http.Server{
 		Addr:         ":8080",
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  120 * time.Second,
-		Handler:      mux,
+		Handler:      router,
 	}
 
 	err := srv.ListenAndServe()
