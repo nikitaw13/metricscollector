539165055ab3fcca57ccda808f6da4165f02fd05
Nikita Walkowski <valkovskii.n@selectel.ru>
2026-08-16 11:54:13 +0300

    refactor metrics: add validate logic

--- internal/model/metrics.go
+++ internal/model/metrics.go
@@ -1,8 +1,16 @@
 package model
 
+import "errors"
+
 const (
 	Counter = "counter"
 	Gauge   = "gauge"
+)
+
+var (
+	ErrInvalidType  = errors.New("Invalid metric type")
+	ErrMissingID    = errors.New("Metric ID is required")
+	ErrMissingValue = errors.New("Metric value is required")
 )
 
 // NOTE: Не усложняем пример, вводя иерархическую вложенность структур.
@@ -11,9 +19,29 @@ const (
 // что бы отличать значение "0", от не заданного значения
 // и соответственно не кодировать в структуру.
 type Metrics struct {
-	ID    string   `json:"id"`
-	MType string   `json:"type"`
-	Delta *int64   `json:"delta,omitempty"`
-	Value *float64 `json:"value,omitempty"`
+	ID    string   `json:"id"`              // имя метрики
+	MType string   `json:"type"`            // параметр, принимающий значение: gauge или counter
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 	Hash  string   `json:"hash,omitempty"`
+}
+
+func (m Metrics) ValidateForUpdate() error {
+	if m.MType != Gauge && m.MType != Counter {
+		return ErrInvalidType
+	}
+	if m.ID == "" {
+		return ErrMissingID
+	}
+	return nil
+}
+
+func (m Metrics) ValidateForRead() error {
+	if m.MType != Gauge && m.MType != Counter {
+		return ErrInvalidType
+	}
+	if m.ID == "" {
+		return ErrMissingID
+	}
+	return nil
 }
