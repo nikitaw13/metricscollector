bc548d8ced0be4e1b4fb22b765ed1c5be2c9900e
Nikita Walkowski <valkovskii.n@selectel.ru>
2026-08-16 18:07:10 +0300

    rename router & use chi.AllowContentType

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -2,26 +2,37 @@ package handler
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func (h *MetricsHandler) New() *chi.Mux {
-	router := chi.NewRouter()
-	router.Use(LoggerMiddleware)
+	r := chi.NewRouter()
+	r.Use(LoggerMiddleware)
 
-	router.Get("/", h.GetRoot)
-	router.Route("/value/{TYPE}", func(r chi.Router) {
+	r.Get("/", h.GetRoot)
+	r.Route("/value/{TYPE}", func(r chi.Router) {
+		r.Use(middleware.AllowContentType("text/plain"))
 		r.Use(TypeMiddleware)
 		r.Get("/{METRIC}", h.GetMetric)
 	})
 
-	router.Post("/value", h.PostValue)
-	router.Post("/update", h.PostUpdate)
-	router.Route("/update/{TYPE}", func(r chi.Router) {
+	r.Route("/update", func(r chi.Router) {
+		r.Use(middleware.AllowContentType("application/json"))
+		r.Post("/", h.PostUpdate)
+	})
+
+	r.Route("/value", func(r chi.Router) {
+		r.Use(middleware.AllowContentType("application/json"))
+		r.Post("/", h.PostValue)
+	})
+
+	r.Route("/update/{TYPE}", func(r chi.Router) {
+		r.Use(middleware.AllowContentType("text/plain"))
 		r.Use(TypeMiddleware)              // проверит TYPE для всех трёх ниже
 		r.Post("/", h.PostNoMetric)        // 404 — нет имени
 		r.Post("/{METRIC}", h.PostNoValue) // 400 — нет значения
 		r.Post("/{METRIC}/{VALUE}", h.PostFull)
 	})
 
-	return router
+	return r
 }
