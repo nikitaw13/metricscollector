e9191a7f69399232f21c23f7501fa996e5ba5705
Nikita Walkowski <valkovskii.n@selectel.ru>
2026-08-16 14:13:06 +0300

    add validation for metrics

--- internal/model/metrics.go
+++ internal/model/metrics.go
@@ -1,6 +1,9 @@
 package model
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 const (
 	Counter = "counter"
@@ -8,9 +11,12 @@ const (
 )
 
 var (
-	ErrInvalidType  = errors.New("Invalid metric type")
-	ErrMissingID    = errors.New("Metric ID is required")
+	ErrMissingType = errors.New("Metric type is required")
+	ErrInvalidType = errors.New("Invalid metric type")
+	ErrMissingID   = errors.New("Metric ID is required")
+	// ErrInvalidID    = errors.New("Invalid ID")
 	ErrMissingValue = errors.New("Metric value is required")
+	ErrInvalidValue = errors.New("Invalid metric value")
 )
 
 // NOTE: Не усложняем пример, вводя иерархическую вложенность структур.
@@ -27,18 +33,33 @@ type Metrics struct {
 }
 
 func (m Metrics) ValidateForUpdate() error {
+	if m.MType == "" {
+		return ErrMissingType
+	}
 	if m.MType != Gauge && m.MType != Counter {
 		return ErrInvalidType
 	}
 	if m.ID == "" {
 		return ErrMissingID
 	}
+
+	if m.MType == Gauge && m.Value == nil || m.MType == Counter && m.Delta == nil {
+		return ErrMissingValue
+	}
+
+	if m.MType == Gauge && m.Value != nil && math.IsInf(*m.Value, 0) {
+		return ErrInvalidValue
+	}
+	if m.MType == Gauge && m.Value != nil && math.IsNaN(*m.Value) {
+		return ErrInvalidValue
+	}
+
 	return nil
 }
 
 func (m Metrics) ValidateForRead() error {
-	if m.MType != Gauge && m.MType != Counter {
-		return ErrInvalidType
+	if m.MType == "" {
+		return ErrMissingType
 	}
 	if m.ID == "" {
 		return ErrMissingID
