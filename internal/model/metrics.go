2441dc9c701e1d95161ac6ecaf0ecb43f5b0ec03
Nikita Walkowski <valkovskii.n@selectel.ru>
2026-08-17 15:51:38 +0300

    refactor: naming in model & add tests

--- internal/model/metrics.go
+++ internal/model/metrics.go
@@ -24,48 +24,48 @@ var (
 // Delta и Value объявлены через указатели,
 // что бы отличать значение "0", от не заданного значения
 // и соответственно не кодировать в структуру.
-type Metrics struct {
+type Metric struct {
 	ID    string   `json:"id"`              // имя метрики
-	MType string   `json:"type"`            // параметр, принимающий значение: gauge или counter
+	Type string   `json:"type"`            // параметр, принимающий значение: gauge или counter
 	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 	Hash  string   `json:"hash,omitempty"`
 }
 
-func (m Metrics) ValidateForUpdate() error {
-	if m.MType == "" {
+func (m Metric) ValidateForUpdate() error {
+	if m.Type == "" {
 		return ErrMissingType
 	}
-	if m.MType != Gauge && m.MType != Counter {
+	if m.Type != Gauge && m.Type != Counter {
 		return ErrInvalidType
 	}
 	if m.ID == "" {
 		return ErrMissingID
 	}
 
-	if (m.MType == Gauge && m.Value == nil) || (m.MType == Counter && m.Delta == nil) {
+	if (m.Type == Gauge && m.Value == nil) || (m.Type == Counter && m.Delta == nil) {
 		return ErrMissingValue
 	}
 
-	if m.MType == Gauge && m.Value != nil && math.IsInf(*m.Value, 0) {
+	if m.Type == Gauge && m.Value != nil && math.IsInf(*m.Value, 0) {
 		return ErrInvalidValue
 	}
-	if m.MType == Gauge && m.Value != nil && math.IsNaN(*m.Value) {
+	if m.Type == Gauge && m.Value != nil && math.IsNaN(*m.Value) {
 		return ErrInvalidValue
 	}
 
-	if m.MType == Counter && m.Delta != nil && *m.Delta < 0 {
+	if m.Type == Counter && m.Delta != nil && *m.Delta < 0 {
 		return ErrInvalidValue
 	}
 
 	return nil
 }
 
-func (m Metrics) ValidateForRead() error {
-	if m.MType == "" {
+func (m Metric) ValidateForRead() error {
+	if m.Type == "" {
 		return ErrMissingType
 	}
-	if m.MType != Gauge && m.MType != Counter {
+	if m.Type != Gauge && m.Type != Counter {
 		return ErrInvalidType
 	}
 	if m.ID == "" {
